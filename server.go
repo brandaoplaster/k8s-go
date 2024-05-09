a67561a9949c0921a38ee4c9fc23a521569995e5
brandaoplaster <brandaoplaster@gmail.com>
2024-05-08 22:45:54 -0300

    feat: Adding func hello

--- server.go
+++ server.go
@@ -1,5 +1,14 @@
 package main
 
+import "net/http"
+
 func main() {
 	println("Hello, World!")
+
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":80", nil)
+}
+
+func Hello(write http.ResponseWriter, request *http.Request) {
+	write.Write([]byte("Hello, World!"))
 }
