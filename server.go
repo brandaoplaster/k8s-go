a55016a0aa8720503df8cdd1911163afb214c201
brandaoplaster <brandaoplaster@gmail.com>
2024-05-10 22:53:47 -0300

    feat: Add func healthz

--- server.go
+++ server.go
@@ -1,14 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var startedAt = time.Now()
 
 func main() {
 	println("Hello, World!")
 
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(write http.ResponseWriter, request *http.Request) {
 	write.Write([]byte("Hello, World!"))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
+
 }
